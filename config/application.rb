5e587a6aad4ddea9ddcc9eedd99dddc5944dbd59
EC2 Default User <ec2-user@ip-172-31-4-201.ap-northeast-1.compute.internal>
2025-05-24 05:57:55 +0000

    config/application.rbでのタイムゾーンやデフォルト言語の設定

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require_relative "boot"
 
 require "rails/all"
 
+# Railsアプリ全体に共通する基本設定を定義。
+# 開発・本番といった環境に依存しない設定。
+# よく使うのは１．タイムゾーン　２．デフォの言語設定　３．rails gで自動的に作られるhelperなどができないように設定
+# ４．カスタム
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +14,10 @@ module Baukis2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.time_zone ="Tokyo"
+    config.i18n.load.load_path += Dir[Rails.root.join("config","locales","**","*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
