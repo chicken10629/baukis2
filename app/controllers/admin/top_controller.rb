eb971c473815efeea9f300c8242f53fdb956bba7
EC2 Default User <ec2-user@ip-172-31-4-201.ap-northeast-1.compute.internal>
2025-05-26 21:59:22 +0000

    コントローラーの基本

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,2 +1,9 @@
 class Admin::TopController < ApplicationController
+  def index
+    # renderやredirectなどを書かなかった場合、自動的にこのアクションに対応するテンプレートが描画。
+    # renderはHTML文書を生成するメソッド。生成にERBというライブラリを使用する。
+    # ERBに対応するファイルは拡張子が.erbとなっている。〇〇.html.erbの形が基本。
+    # 原則、ERBテンプレートはviewsディレクトリにコントローラー名ディレクトリ/アクション名ファイルとなる
+    render action: "index"
+  end
 end
