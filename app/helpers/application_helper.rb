1e0a95779091ebbea75725d224d3aba3ff2d2dc8
EC2 Default User <ec2-user@ip-172-31-4-201.ap-northeast-1.compute.internal>
2025-05-28 21:36:42 +0000

    部分テンプレート、ヘルパーメソッド

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # blank?は①長さが0 ②全ての文字が空白文字（スペースや改行など） ③配列の要素が0 ④nilの時true
+  # present?は中身がある,nilではないときtrue
+  def document_title
+    if @title.present?
+      "#{@title} - Baukis2"
+    else
+      "Baukis2"
+    end
+  end
 end
